3f65d05b3ef88a83acce87dee8a222ffabd99972
Anton Troshin <11165918+grauds@users.noreply.github.com>
2024-02-21 21:26:35 +0300

    [+] Removed unused dependency

--- libs/installer/src/test/java/jworkspace/installer/WorkspaceInstallerTest.java
+++ libs/installer/src/test/java/jworkspace/installer/WorkspaceInstallerTest.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-
 import org.junit.rules.TemporaryFolder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
